0dde411a125dba1a9c4d6b5cc31fa2655ca7ad72
Rodrigo Moral Fiel <rmoralf@gmail.com>
2016-09-16 11:52:00 +0200

    Package rename and library settings

--- touchvie_front/src/main/java/com/touchvie/touchvie_front/ui/activities/Carousel.java
+++ touchvie_front/src/main/java/com/touchvie/touchvie_front/ui/activities/Carousel.java
@@ -1,10 +1,9 @@
 package com.touchvie.touchvie_front.ui.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.touchvie.touchvie_front.R;
-
 
 public class Carousel extends AppCompatActivity {
 
