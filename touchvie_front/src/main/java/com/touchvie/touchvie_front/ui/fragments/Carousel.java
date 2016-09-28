171cd3dc93d8ef180a2b67105af9af9b234a8c2f
Rodrigo Moral Fiel <rmoralf@gmail.com>
2016-09-28 11:05:48 +0200

    Relations added to testing carousel cards

--- touchvie_front/src/main/java/com/touchvie/touchvie_front/ui/fragments/Carousel.java
+++ touchvie_front/src/main/java/com/touchvie/touchvie_front/ui/fragments/Carousel.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.touchvie.backend.Card;
+import com.touchvie.backend.Relation;
 import com.touchvie.touchvie_client.data.CarouselCard;
 import com.touchvie.touchvie_front.R;
 import com.touchvie.touchvie_front.data.Scene;
@@ -192,45 +193,115 @@ public class Carousel extends Fragment implements CarouselCardListener, FastScro
 
     private AbstractFlexibleItem generateRandomCarouselCard(SceneHeaderItem sceneHeader, int j) {
         Card data = new Card();
+        CarouselCard temp = new CarouselCard();
         Random r = new Random();
         int rnd = r.nextInt(8);
         switch (rnd) {
             case 0:
                 data.setTitle("Ferrari");
                 data.setImage("http://i.imgur.com/7L1egba.jpg");
+                temp.setData(data);
                 return new CarouselItemGeneric(j + 1, sceneHeader, data);
             case 1:
                 data.setTitle("Mansión");
                 data.setImage("http://i.imgur.com/vttzfn4.jpg");
                 return new CarouselItemGeneric(j + 1, sceneHeader, data);
             case 2:
-                data.setTitle("Suiza");
-                data.setImage("http://i.imgur.com/iliue4l.jpg");
+                data.setTitle("Jordan Belfort");
+                data.setImage("http://i.imgur.com/MQ1SWyh.jpg");
+                Relation rel1 = new Relation();
+                Card relCard1 = new Card();
+                relCard1.setTitle("Traje Armani");
+                relCard1.setImage("http://i.imgur.com/kNjXkKy.jpg");
+                Relation rel2 = new Relation();
+                Card relCard2 = new Card();
+                relCard2.setTitle("Zapatos");
+                relCard2.setImage("http://i.imgur.com/4W8uL80.jpg");
+                Relation[] rels = new Relation[2];
+                rels[0] = rel1;
+                rels[1] = rel2;
+                temp.setChildren(rels);
                 return new CarouselItemGeneric(j + 1, sceneHeader, data);
             case 3:
-                data.setTitle("Los 80");
-                data.setImage("http://i.imgur.com/EOSf9EC.jpg");
+                data.setTitle("Donnie Azoff");
+                data.setImage("http://i.imgur.com/HWAxhwP.png");
+                Relation rel10 = new Relation();
+                Card relCard10 = new Card();
+                relCard10.setTitle("Camisa");
+                relCard10.setImage("http://i.imgur.com/9yN9vse.jpg");
+                Relation[] rels4 = new Relation[1];
+                rels4[0] = rel10;
+                temp.setChildren(rels4);
                 return new CarouselItemGeneric(j + 1, sceneHeader, data);
             case 4:
-                data.setTitle("Nueva York");
-                data.setImage("http://i.imgur.com/7xlFXFY.gif");
+                data.setTitle("Yate");
+                data.setImage("http://i.imgur.com/ZEjJ2T6.jpg");
+                Relation rel9 = new Relation();
+                Card relCard9 = new Card();
+                relCard9.setTitle("Puerto");
+                relCard9.setImage("http://i.imgur.com/mRhNiUd.jpg");
+                Relation[] rels5 = new Relation[1];
+                rels5[0] = rel9;
+                temp.setChildren(rels5);
                 return new CarouselItemGeneric(j + 1, sceneHeader, data);
             case 5:
+                data.setTitle("Wall Street");
+                data.setImage("http://i.imgur.com/TsqenDf.jpg");
+                Relation rel7 = new Relation();
+                Card relCard7 = new Card();
+                relCard7.setTitle("Bolsa de NY");
+                relCard7.setImage("http://i.imgur.com/agM641t.jpg");
+                Relation rel8 = new Relation();
+                Card relCard8 = new Card();
+                relCard8.setTitle("Acciones a centavo");
+                relCard8.setImage("http://i.imgur.com/LIWqjvB.jpg");
+                Relation[] rels3 = new Relation[2];
+                rels3[0] = rel7;
+                rels3[1] = rel8;
+                temp.setChildren(rels3);
+                return new CarouselItemGeneric(j + 1, sceneHeader, data);
+            case 6:
+                data.setTitle("Curiosity card test");
+                data.setImage("http://i.imgur.com/vq92crr.jpg");
+                return new CarouselItemCuriosity(j + 1, sceneHeader, data);
+            case 7:
                 data.setTitle("¿Sabías qué...?");
                 data.setImage("http://i.imgur.com/sVJbVDv.jpg");
                 return new CarouselItemCuriosity(j + 1, sceneHeader, data);
-            case 6:
+            case 8:
+                data.setTitle("Nueva York");
+                data.setImage("http://i.imgur.com/7xlFXFY.gif");
+                return new CarouselItemGeneric(j + 1, sceneHeader, data);
+            case 9:
+                data.setTitle("Los 80");
+                data.setImage("http://i.imgur.com/EOSf9EC.jpg");
+                return new CarouselItemGeneric(j + 1, sceneHeader, data);
+            case 10:
+                data.setTitle("Mark Hanna");
+                data.setImage("http://i.imgur.com/wJYjSyO.jpg");
+                Relation rel3 = new Relation();
+                Card relCard3 = new Card();
+                relCard3.setTitle("Traje");
+                relCard3.setImage("http://i.imgur.com/RdllUq3.jpg");
+                Relation rel4 = new Relation();
+                Card relCard4 = new Card();
+                relCard4.setTitle("Zapatos");
+                relCard4.setImage("http://i.imgur.com/rms7VaC.jpg");
+                Relation[] rels2 = new Relation[4];
+                rels2[0] = rel3;
+                rels2[1] = rel4;
+                temp.setChildren(rels2);
+                return new CarouselItemGeneric(j + 1, sceneHeader, data);
+            case 11:
                 data.setTitle("Brad");
                 data.setImage("http://i.imgur.com/1LQggOl.jpg");
-                return new CarouselItemCuriosity(j + 1, sceneHeader, data);
-            case 7:
+                return new CarouselItemGeneric(j + 1, sceneHeader, data);
+            case 12:
                 data.setTitle("Helicóptero");
                 data.setImage("http://i.imgur.com/vq92crr.jpg");
-                return new CarouselItemCuriosity(j + 1, sceneHeader, data);
+                return new CarouselItemGeneric(j + 1, sceneHeader, data);
         }
         return null;
     }
-
-
 }
 
