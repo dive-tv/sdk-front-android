ad66864d5e027811c76d3274120c2d6d85020403
nleon-development <nleofer@gmail.com>
2016-10-27 18:35:54 +0200

    relation rel types

---
+++ touchvie_front/src/main/java/com/touchvie/backend/carddetail/relations/RelationRelTypes.java
@@ -0,0 +1,24 @@
+package com.touchvie.backend.carddetail.relations;
+
+/**
+ * Created by Tagsonomy S.L. on 27/10/2016.
+ */
+
+public class RelationRelTypes {
+    public final static String FEATURES_TRIVIA="features_trivia";
+    public final static String FEATURES_VEHICLE="features_vehicle";
+    public final static String FEATURES_="features_*";
+    public final static String IS_SIMILAR_TO ="is_similar_to";
+    public final static String DIRECTED_BY="directed_by";
+    public final static String _ON_MOVIE="*_on_movie";
+    public final static String IS_PART_OF="is_part_of";
+    public final static String COMPOSED_OF="composed_of";
+    public final static String CONTAINS_SONG="contains_song";
+    public final static String OST_ON_MOVIE="ost_on_movie";
+    public final static String IS_PART_OF_OST="is_part_of_ost";
+    public final static String PLAYS="plays";
+    public final static String FILMED_IN="filmed_in";
+    public final static String REPRESENTS="represents";
+    public final static String FEATURES_location="features_location";
+    public final static String WEARS="wears";
+}
